9b85019b741514ae028cfa920ef29472aca71ba0
sylvia villegas <sylvia@svillegas.com>
2017-01-18 22:37:45 -0500

    Add name argument to initialize

--- rover.rb
+++ rover.rb
@@ -1,7 +1,7 @@
 class Rover
   attr_accessor :name :x_coord, :y_coord, :bearing
 
-  def initialize(x_coord, y_coord, bearing)
+  def initialize(name, x_coord, y_coord, bearing)
     @name = name
     @x_coord = x_coord
     @y_coord = y_coord
