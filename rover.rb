8a9860a99931d7bebde0e298613ae223eb13c1a2
sylvia villegas <sylvia@svillegas.com>
2017-01-19 00:20:10 -0500

    Add input prompts (has bugs)

--- rover.rb
+++ rover.rb
@@ -1,15 +1,15 @@
 class Rover
-  attr_accessor :name :x_coord, :y_coord, :bearing
+  attr_accessor :name, :x_coordinate, :y_coordinate, :bearing
 
-  def initialize(name, x_coord, y_coord, bearing)
+  def initialize(name, x_coordinate, y_coordinate, bearing)
     @name = name
-    @x_coord = x_coord
-    @y_coord = y_coord
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
     @bearing = bearing
   end
 
   def read_instruction(instruction)
-    @instruction.each_char do |char|
+    instruction.each_char do |char|
       if char == "L"
         turn("L")
       elsif char == "R"
@@ -49,32 +49,54 @@ class Rover
   def move
     case @bearing
     when "N"
-      @y_coord += 1
+      @y_coordinate += 1
     when "E"
-      @x_coord += 1
+      @x_coordinate += 1
     when "S"
-      @y_coord -= 1
+      @y_coordinate -= 1
     when "W"
-      @x_coord -= 1
+      @x_coordinate -= 1
     end
   end
 
   def report
-    puts "Current position is #{@x_coord} #{@y_coord} #{@bearing}"
+    puts "Current position of #{@name} is #{@x_coordinate} #{@y_coordinate} #{@bearing.upcase}"
   end
 
-end
+end # class Rover
 
 # TEST:
-# spirit = Rover.new(1, 2, "N")
+# spirit = Rover.new("Spirit", 1, 2, "N")
 # spirit.read_instruction("LMLMLMLMM")
 # spirit.report
 #
-# opportunity = Rover.new(3, 3, "E")
+# opportunity = Rover.new("Opportunity", 3, 3, "E")
 # opportunity.read_instruction("MMRMMRMRRM")
 # opportunity.report
 
 # TODO:
-# - Accept 5 inputs with [gets]
+# - Accept 5 inputs with [gets]; see below, broken
 # - Mission Control class
 # - Plateau class
+
+puts "Rover name:"
+name = gets.chomp
+
+puts "West-to-East position, in grid units:"
+x_coordinate = gets.chomp
+
+puts "South-to-North position, in grid units:"
+y_coordinate = gets.chomp
+
+# BUG: Throws errors w/ capital letter input
+puts "Bearing (N, E, S, W):"
+bearing = gets.chomp
+
+rover1 = Rover.new(name, x_coordinate, y_coordinate, bearing)
+
+puts "Complete instructions for travel (L for left, R for right, M to move forward 1 grid unit):"
+given_instructions = gets.chomp
+
+# BUG: Not returning updated position
+rover1.read_instruction(given_instructions)
+rover1.report
