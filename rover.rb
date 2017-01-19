4b1b60a63619f6690d9a4097e25f0b751a0c30dd
sylvia villegas <sylvia@svillegas.com>
2017-01-18 20:48:27 -0500

    Create and initialize class Rover

---
+++ rover.rb
@@ -0,0 +1,10 @@
+class Rover
+  attr_accessor :x_coord, :y_coord, :bearing
+
+  def initialize(x_coord, y_coord, bearing)
+    @x_coord = x_coord
+    @y_coord = y_coord
+    @bearing = bearing
+  end
+
+end
