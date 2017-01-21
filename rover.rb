4c01076f4b643c05fb88aeb89aff93619f96c2d3
sylvia villegas <sylvia@svillegas.com>
2017-01-21 13:26:31 -0500

    Add user input for position,instructions

--- rover.rb
+++ rover.rb
@@ -1,14 +1,32 @@
 class Rover
-  attr_accessor :name, :x_coordinate, :y_coordinate, :bearing
+  attr_reader :name
+  attr_accessor :x_coordinate, :y_coordinate, :bearing
 
-  def initialize(name, x_coordinate, y_coordinate, bearing)
+  def initialize(name)
     @name = name
-    @x_coordinate = x_coordinate
-    @y_coordinate = y_coordinate
-    @bearing = bearing
+    @x_coordinate = 0
+    @y_coordinate = 0
+    @bearing = "N"
+    start_rover
   end
 
-  def read_instruction(instruction)
+  def start_rover
+    puts "Position of rover?\n(x & y coordinates and bearing, e.g. 3 1 W)"
+    position = gets.chomp
+    position = position.split(" ")
+    @x_coordinate = position[0].to_i
+    @y_coordinate = position[1].to_i
+    @bearing = position[2]
+    # get_instructions
+  end
+
+  def get_instructions
+    puts "Instructions for rover?\n(L = left, R = right, M = move forward, e.g. LLMMRMLMMR)"
+    rover_instructions = gets.chomp
+    follow_instruction(rover_instructions)
+  end
+
+  def follow_instruction(instruction)
     instruction.each_char do |char|
       if char == "L"
         turn("L")
@@ -18,12 +36,13 @@ class Rover
         move
       end
     end
+    report
   end
 
   def turn(direction)
     if direction == "L"
       case @bearing
-      when "N"
+      when "N" || "n"
         @bearing = "W"
       when "W"
         @bearing = "S"
@@ -60,13 +79,18 @@ class Rover
   end
 
   def report
-    puts "Current position of #{@name} is #{@x_coordinate} #{@y_coordinate} #{@bearing.upcase}"
+    puts "Current position of #{name} is #{x_coordinate} #{y_coordinate} #{bearing.upcase}"
+  end
+
+  def move_by1
+    @y_coordinate += 1
   end
 
 end # class Rover
 
 # TEST:
-# spirit = Rover.new("Spirit", 1, 2, "N")
+# spirit = Rover.new("Spirit")
+# spirit.turn("L")
 # spirit.read_instruction("LMLMLMLMM")
 # spirit.report
 #
@@ -74,29 +98,9 @@ end # class Rover
 # opportunity.read_instruction("MMRMMRMRRM")
 # opportunity.report
 
+
+
 # TODO:
-# - Accept 5 inputs with [gets]; see below, broken
+# - Accept 5 inputs with [gets]; I've got 2...
 # - Mission Control class
 # - Plateau class
-
-puts "Rover name:"
-name = gets.chomp
-
-puts "West-to-East position, in grid units:"
-x_coordinate = gets.chomp
-
-puts "South-to-North position, in grid units:"
-y_coordinate = gets.chomp
-
-# BUG: Throws errors w/ capital letter input
-puts "Bearing (N, E, S, W):"
-bearing = gets.chomp
-
-rover1 = Rover.new(name, x_coordinate, y_coordinate, bearing)
-
-puts "Complete instructions for travel (L for left, R for right, M to move forward 1 grid unit):"
-given_instructions = gets.chomp
-
-# BUG: Not returning updated position
-rover1.read_instruction(given_instructions)
-rover1.report
